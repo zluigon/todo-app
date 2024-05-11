b33cf52b48b00658e67bdfcded2935b5a5d88f66
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2024-05-10 20:32:48 -0400

    update: refactored todo list, implemented use of local storage to persist data, implement edit feature to update tasks

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,16 @@ import TODOHero from "@/components/TODOHero";
 import TODOList from "@/components/TODOList";
 
 function Home() {
+	const [todos, setTodos] = React.useState([]);
+	const todos_completed = todos.filter((todo) => todo.is_completed).length;
+	const total_todos = todos.length;
+
 	return (
 		<div className="wrapper">
 			<Header />
-			<TODOHero todos_completed={0} total_todos={0} />
-			<Form />
-			<TODOList todos={[]} />
+			<TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+			<Form todos={todos} setTodos={setTodos} />
+			<TODOList todos={todos} setTodos={setTodos} />
 		</div>
 	);
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,11 @@
-function Form() {
+function Form({ setTodos }) {
 	const handleSubmit = (event) => {
 		event.preventDefault();
+		const value = event.target.todo.value;
+		setTodos((prevTodos) => [
+			...prevTodos,
+			{ title: value, id: self.crypto.randomUUID(), is_completed: false },
+		]);
 		event.target.reset();
 	};
 
