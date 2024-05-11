9ca191bd9bfe86c8da8e3f5d2a45fd1f7e5c5f28
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2024-05-10 20:37:51 -0400

    update: implemented reading of local storage to load tasks

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
-
 import React from "react";
+
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import TODOHero from "@/components/TODOHero";
@@ -8,13 +8,26 @@ import TODOList from "@/components/TODOList";
 
 function Home() {
 	const [todos, setTodos] = React.useState([]);
-	const todos_completed = todos.filter((todo) => todo.is_completed).length;
-	const total_todos = todos.length;
 
+	// Retrieve data from localStorage when component mounts
+	React.useEffect(() => {
+		const storedTodos = localStorage.getItem("todos");
+		if (storedTodos) {
+			setTodos(JSON.parse(storedTodos));
+		}
+	}, []);
+
+	const todos_completed = todos.filter(
+		(todo) => todo.is_completed == true
+	).length;
+	const total_todos = todos.length;
 	return (
 		<div className="wrapper">
 			<Header />
-			<TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+			<TODOHero
+				todos_completed={todos_completed}
+				total_todos={total_todos}
+			/>
 			<Form todos={todos} setTodos={setTodos} />
 			<TODOList todos={todos} setTodos={setTodos} />
 		</div>
