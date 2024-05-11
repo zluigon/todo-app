890859904d5e618f410e69d3c6868284f8619aac
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2024-05-10 20:11:38 -0400

    update: applied style classes to components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,41 @@
-'use client';
+"use client";
 
-function Form(){
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        event.target.reset();
-    };
+function Form() {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		event.target.reset();
+	};
 
-    return (
-        <form className="form" onSubmit={handleSubmit}>
-            <label htmlFor="todo">
-                <input type="text" id="todo" placeholder="Write your next task" required />
-            </label>
-            <button>
-                <span className="visually-hidden">Submit</span>
-                <svg>
-                    <path d=""/>
-                </svg>
-            </button>
-        </form>
-    );
+	return (
+		<form className="form" onSubmit={handleSubmit}>
+			<label htmlFor="todo">
+				<input
+					type="text"
+					id="todo"
+					placeholder="Write your next task"
+					required
+				/>
+			</label>
+			<button>
+				<span className="visually-hidden">Submit</span>
+				<svg
+					clipRule="evenodd"
+					fillRule="evenodd"
+					strokeLinejoin="round"
+					strokeMiterlimit="2"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					width={32}
+					height={32}
+				>
+					<path
+						d="m11 11h-7.25c-.414 0-.75.336-.75.75s.336.75.75.75h7.25v7.25c0 .414.336.75.75.75s.75-.336.75-.75v-7.25h7.25c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-7.25v-7.25c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+						fillRule="nonzero"
+					/>
+				</svg>
+			</button>
+		</form>
+	);
 }
 
 export default Form;
--- src/components/TODOHero.jsx
+++ src/components/TODOHero.jsx
@@ -1,9 +1,9 @@
 function TODOHero({todos_completed, total_todos}) {
     return (
-        <section>
+        <section className="todohero_section">
             <div>
-                <p>Task Done</p>
-                <p>Keep it up</p>
+                <p className="text_large">Task Done</p>
+                <p className="text_small">Keep it up</p>
             </div>
             <div>
                 {todos_completed}/{total_todos}
