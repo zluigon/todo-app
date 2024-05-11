43296c5fddb28784f5d7a0066645330e2500d4bd
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2024-05-10 20:14:10 -0400

    fix: refactored app to specify client components

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Form from "@/components/Form";
 import Header from "@/components/Header";
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 function Form() {
 	const handleSubmit = (event) => {
 		event.preventDefault();
